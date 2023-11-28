083ff23d644285987639bfef319ed2cd099ffeaf
Danila <115975173+Danila331@users.noreply.github.com>
2023-11-28 22:31:23 +0300

    feat: Реализовал сервис для рекламных постов

--- internal/services/advertisement.go
+++ internal/services/advertisement.go
@@ -1,6 +1,8 @@
 package services
 
 import (
+	"fmt"
+
 	"github.com/Troom-Corp/troom/internal/storage"
 )
 
@@ -8,13 +10,13 @@ type AdvertisementInterface interface {
 	Create() error
 	Update() error
 	ReadAll() ([]Advertisement, error)
-	ReadById(AdvertId int) (Advertisement, error)
+	ReadById() (Advertisement, error)
 	Delete() error
 }
 
 type Advertisement struct {
 	AdvertId  int
-	UserId    int
+	AuthorId  int
 	Time      string
 	Title     string
 	Blocks    string
@@ -28,5 +30,100 @@ func (a Advertisement) Create() error {
 		conn.Close()
 		return err
 	}
-	createQuery := "INSERT INTO public.advertisment (advertid, userid, time, title, blocks, starttime, endtime) VALUES (%d, %d, '%s', '%s', '%s', '%s', '%s')"
+	createQuery := fmt.Sprintf("INSERT INTO public.advertisement (advertid, authorid, time, title, blocks, starttime, endtime) VALUES (%d, %d, '%s', '%s', '%s', '%s', '%s')",
+		a.AdvertId,
+		a.AuthorId,
+		a.Time,
+		a.Title,
+		a.Blocks,
+		a.StartTime,
+		a.EndTime)
+	_, err = conn.Query(createQuery)
+	if err != nil {
+		conn.Close()
+		return err
+	}
+	conn.Close()
+	return nil
+}
+
+func (a Advertisement) Update() error {
+	conn, err := storage.Sql.Open()
+
+	if err != nil {
+		conn.Close()
+		return err
+	}
+
+	updateQuery := fmt.Sprintf("UPDATE public.advertisement SET title = '%s' blocks = '%s' WHERE advertid = %d",
+		a.Title,
+		a.Blocks,
+		a.AdvertId)
+
+	_, err = conn.Query(updateQuery)
+
+	if err != nil {
+		conn.Close()
+		return err
+	}
+
+	conn.Close()
+	return nil
+}
+
+func (a Advertisement) ReadAll() ([]Advertisement, error) {
+	var Advertisments []Advertisement
+	conn, err := storage.Sql.Open()
+
+	if err != nil {
+		conn.Close()
+		return []Advertisement{}, err
+	}
+
+	readAllQuery := fmt.Sprintf("SELECT * FROM public.advertisement")
+	conn.Select(&Advertisments, readAllQuery)
+
+	conn.Close()
+	return Advertisments, nil
+}
+
+func (a Advertisement) ReadById() (Advertisement, error) {
+	var Advert Advertisement
+	conn, err := storage.Sql.Open()
+
+	if err != nil {
+		conn.Close()
+		return Advertisement{}, nil
+	}
+
+	readByIdQuery := fmt.Sprintf("SELECT * FROM public.advertisement WHERE advertid = %d", a.AdvertId)
+	err = conn.Get(&Advert, readByIdQuery)
+
+	if err != nil {
+		conn.Close()
+		return Advertisement{}, nil
+	}
+
+	conn.Close()
+	return Advert, nil
+}
+
+func (a Advertisement) Delete() error {
+	conn, err := storage.Sql.Open()
+
+	if err != nil {
+		conn.Close()
+		return err
+	}
+
+	deleteQuery := fmt.Sprintf("DELETE FROM public.advertisement WHERE advertid = %d", a.AdvertId)
+	_, err = conn.Query(deleteQuery)
+
+	if err != nil {
+		conn.Close()
+		return err
+	}
+
+	conn.Close()
+	return nil
 }
