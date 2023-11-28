2274de9aa0d1aa0590a872f8d43dd7af8257f8fb
Danila <petruxa.tv@yandex.ru>
2023-11-28 20:45:44 +0300

    Update advertisement.go

--- internal/services/advertisement.go
+++ internal/services/advertisement.go
@@ -1,8 +1,32 @@
 package services
 
+import (
+	"github.com/Troom-Corp/troom/internal/storage"
+)
+
 type AdvertisementInterface interface {
+	Create() error
+	Update() error
+	ReadAll() ([]Advertisement, error)
+	ReadById(AdvertId int) (Advertisement, error)
+	Delete() error
 }
 
 type Advertisement struct {
-	AdvertId int
+	AdvertId  int
+	UserId    int
+	Time      string
+	Title     string
+	Blocks    string
+	StartTime string
+	EndTime   string
+}
+
+func (a Advertisement) Create() error {
+	conn, err := storage.Sql.Open()
+	if err != nil {
+		conn.Close()
+		return err
+	}
+	createQuery := "INSERT INTO public.advertisment (advertid, userid, time, title, blocks, starttime, endtime) VALUES (%d, %d, '%s', '%s', '%s', '%s', '%s')"
 }
